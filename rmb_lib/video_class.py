190e61c957ce3c7587d5be977761b97be31c6c9b
Rhythmblue <zhouhaocomm@qq.com>
2017-09-28 11:29:23 +0800

    0

--- rmb_lib/video_class.py
+++ rmb_lib/video_class.py
@@ -2,13 +2,15 @@ import random
 import os
 import numpy as np
 from PIL import Image
-from data_augment import transform_data
+from rmb_lib.data_augment import transform_data
 
 
 class Video_3D:
     def __init__(self, info_list, tag='rgb', img_format='frame{:06d}{}.jpg'):
         '''
             info_list: [name, path, total_frame, label]
+            tag: 'rgb'(default) or 'flow'
+            img_format: 'frame{:06d}{}.jpg'(default)
         '''
         self.name = info_list[0]
         self.path = info_list[1]
@@ -24,18 +26,19 @@ class Video_3D:
         self.img_format = img_format
 
     def get_frames(self, frame_num, side_length=224, is_numpy=True):
+        assert frame_num <= self.total_frame_num
         frames = list()
         start = random.randint(1, self.total_frame_num-frame_num+1)
         for i in range(start, start+frame_num):
             frames.extend(self.load_img(i))
         frames = transform_data(frames)
-        
+    
         if is_numpy:
-            if self.tag=='rgb':
+            if self.tag == 'rgb':
                 frames_np = np.zeros(shape=(frame_num, side_length, side_length, 3))
                 for i, img in enumerate(frames):
                     frames_np[i] = np.asarray(img)
-            elif self.tag=='flow':
+            elif self.tag == 'flow':
                 frames_np = np.zeros(shape=(frame_num, side_length, side_length, 2))
                 for i in range(0, len(frames), 2):
                     tmp = np.stack([np.asarray(frames[i]), np.asarray(frames[i+1])], axis=2)
@@ -44,16 +47,17 @@ class Video_3D:
 
         return  frames
 
-    
+  
     def load_img(self, index):
         img_dir = self.path
         if self.tag == 'rgb':
-            return [Image.open(os.path.join(img_dir,self.img_format.format(index, ''))).convert('RGB')]
+            return [Image.open(os.path.join(img_dir, self.img_format.format(index, ''))).convert('RGB')]
         if self.tag == 'flow':
-            u_img = Image.open(os.path.join(img_dir,self.img_format.format(index, '_u'))).convert('L')
-            v_img = Image.open(os.path.join(img_dir,self.img_format.format(index, '_v'))).convert('L')
+            u_img = Image.open(os.path.join(img_dir, self.img_format.format(index, '_u'))).convert('L')
+            v_img = Image.open(os.path.join(img_dir, self.img_format.format(index, '_v'))).convert('L')
             return [u_img,v_img]
         return
 
     def __str__(self):
-        return 'Video_3D:\nname: {:s}\nframes: {:d}\nlabel: {:d}\nPath: {:s}\n'.format(self.name, self.total_frame_num, self.label, self.path)
+        return 'Video_3D:\nname: {:s}\nframes: {:d}\nlabel: {:d}\nPath: {:s}'.format(
+            self.name, self.total_frame_num, self.label, self.path)
